5878dfc5f633cc6e78f240f5f15664fa158eb958
nodens <micha.pick@gmail.com>
2025-09-18 00:44:48 +0200

    add features and stuff

--- packages/vnc_news/Configuration/TCA/Overrides/pages.php
+++ packages/vnc_news/Configuration/TCA/Overrides/pages.php
@@ -4,6 +4,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 ExtensionManagementUtility::registerPageTSConfigFile(
     'vncnews',
-    'Configuration/TsConfig/Page/LinkBrowser/NewsImageLinkHandler.tsconfig',
+    'Configuration/TsConfig/Page/page.tsconfig',
     'EXT:vnc_news :: Enable images in News RTE'
 );
